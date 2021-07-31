30d18d984587bbe7b1929b90ab1924a1d8d4808d
luis mayta <slovacus@gmail.com>
2021-07-31 11:37:26 -0500

    chore: implement gatsby with mdx (#1)

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pathPrefix: '/slides-git-flow-developer',
+  plugins: [
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-theme-mdx-deck',
+      options: {
+        basePath: ''
+      }
+    }
+  ]
+}
